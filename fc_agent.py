fdc43b42d7df42baf19d6b9f4d94b636e867f1c5
Abay Bektursun <abaybektursun@gmail.com>
2018-05-22 04:31:22 -0500

    sync

--- fc_agent.py
+++ fc_agent.py
@@ -23,7 +23,84 @@ print(device)
 
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
-class ReplayMemory(object):
+class _utils:
+    def __init__(self, board_h, board_w):
+        self.board_area = board_h * board_w 
+
+        self.int2vec = {
+            1  : np.zeros((board_area,)),
+            2  : np.zeros((board_area,)),
+            4  : np.zeros((board_area,)),
+            6  : np.zeros((board_area,)),
+            7  : np.zeros((board_area,)),
+            8  : np.zeros((board_area,)),
+            10 : np.zeros((board_area,)),
+            11 : np.zeros((board_area,)),
+            12 : np.zeros((board_area,)),
+            13 : np.zeros((board_area,))
+        }
+        self.blast_strength_vec = np.zeros((max(board_h, board_w)+1,))
+
+        self.max_ammo = 10
+        self.ammo = np.zeros((self.max_ammo,))
+
+        self.this_agent = np.zeros((5,))
+        self.friend = np.zeros((5,))
+        self.enemy1 = np.zeros((5,))
+        self.enemy2 = np.zeros((5,))
+        self.enemy3 = np.zeros((5,))
+
+        # Different symbolic objects
+        self.input_size = board_area*len(self.int2vec) + self.max_ammo + 5*5
+   
+   def input(self, board):
+        blast_strength = int(obs['blast_strength'])
+        ammo        = int(obs['ammo'])
+        my_position = tuple(obs['position'])
+        teammate    = int(constants.Item(obs['teammate'])) - 9
+        enemies     = np.array([constants.Item(e) for e in obs['enemies']]) - 9
+        board       = np.array(obs['board'])
+        bombs       = np.array(obs['bomb_blast_strength'])
+        bombs_life  = np.array(obs['bomb_life'])
+        
+        # Symbolic objects to vector of boards
+        for idx, cell in enumerate(board.flatten):
+            if cell in self.int2vec:
+                self.int2vec[cell][idx] = 1.0
+        
+        # !TODO Test this assumption
+        self.blast_strength_vec[self.blast_strength] = 1.0
+
+        # If ammo > 10, ammo = 10 (as one hot)
+        self.ammo[min(self.max_ammo,ammo)-1] = 1.0
+
+        agent_ids = [1,2,3,4,5]
+        # Agents
+        for an_enemy_id, an_enemy_vec in zip(enemies, [self.enemy1, self.enemy2, self.enemy3]):
+            an_enemy_vec[an_enemy_id] = 1.0
+            agent_ids.remove(an_enemy_id)
+        self.friend[teammate] = 1.0 
+        agent_ids.remove(teammate)
+        # DEBUG
+        if len(agent_ids) != 1: raise ValueError('Error! agent_ids has more that one id left!')
+        # DEBUG
+        self.this_agent[agent_ids[0]] = 1.0
+
+        # !TODO Figure out 'bomb_blast_strength' and 'bomb_life' maximums to normalize their matrices
+        # !TODO Concatenate all the vectors and return them
+        # !TODO Make sure self.input_size is correct
+
+
+
+        
+
+
+        
+
+            
+
+
+class _ReplayMemory(object):
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
@@ -48,11 +125,12 @@ class ReplayMemory(object):
 class FCAgent(BaseAgent):
     def __init__(self, board_h=11, board_w=11, *args, **kwargs):
         super(FCAgent, self).__init__(*args, **kwargs)
+        # Common functionalities among learning agents
+        self.utils = _utils(board_h, board_w)
+        self.input_size = self.utils.input_size
+
         # Network -----------------------------------------------------------------------
-        #board_size = board_h*board_w
-        # 11 Different symbolic objects
-        board_size = (board_h**2)*3
-        N, D_in, H1, H2, D_out = 1, board_size, 128, 64, 6
+        N, D_in, H1, H2, D_out = 1, input_size, 128, 64, 6
 
         self.model = torch.nn.Sequential(
             torch.nn.Linear(D_in, H1),
@@ -73,7 +151,7 @@ class FCAgent(BaseAgent):
         self.target_net.eval()
 
         self.optimizer = optim.RMSprop(self.policy_net.parameters())
-        self.memory = ReplayMemory(10000)
+        self.memory = _ReplayMemory(10000)
 
         # Hyper Params ------------------------------------------------------------------
         self.BATCH_SIZE = 128
@@ -137,20 +215,8 @@ class FCAgent(BaseAgent):
             #print(torch.tensor([[random.randrange(6)]], device=device, dtype=torch.long))
             return torch.tensor([[random.randrange(2)]], device=device, dtype=torch.long)
     
-    def _onehots(self, board):
-        return 
-        #onehot = np.zeros(1)
-        #for cell in board.flatten
-            
     
     def act(self, obs, action_space):
-        blast_strength = int(obs['blast_strength'])
-        my_position = tuple(obs['position'])
-        enemies     = [constants.Item(e) for e in obs['enemies']]
-        board       = np.array(obs['board'])
-        bombs       = np.array(obs['bomb_blast_strength'])
-        bombs_life  = np.array(obs['bomb_life'])
-        ammo        = int(obs['ammo'])
         
         # Do the one hot thing
         # !TODO
