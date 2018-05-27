45d1ae2edd5580af9e847d346f6ddc61117740bf
Abay Bektursun <abaybektursun@gmail.com>
2018-05-27 02:36:40 -0500

    fresh q

--- fc_agent.py
+++ fc_agent.py
@@ -22,6 +22,8 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 
+from torch.autograd import Variable
+
 from utils import _utils
 
 
@@ -56,7 +58,6 @@ class FCAgent(BaseAgent):
         # Common functionalities among learning agents
         self.utils = _utils(board_h, board_w, 'fc_agent/save.tar')
         self.input_size = self.utils.input_size
-        self.prev_x_np = None
 
         # Network -----------------------------------------------------------------------
         N, D_in, H1, H2, D_out = 1, self.input_size, 128, 64, 6
@@ -68,92 +69,56 @@ class FCAgent(BaseAgent):
             torch.nn.ReLU(),
             torch.nn.Linear(H2, D_out),
         )
-        self.loss = torch.nn.CrossEntropyLoss()
-        self.learning_rate = 1e-4
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
-        #--------------------------------------------------------------------------------
-        
-        self.step_num = 0
-        self.policy_net = self.model.to(device)
-        self.target_net = self.model.to(device)
-        self.target_net.load_state_dict(self.policy_net.state_dict())
-        self.target_net.eval()
         # Hyper Params ------------------------------------------------------------------
-        self.BATCH_SIZE = 128
-        self.GAMMA      = 0.999
-        self.EPS_START  = 0.9
-        self.EPS_END    = 0.05
-        self.EPS_DECAY  = 20_0
-        self.TARGET_UPDATE = 10
+        # Hyper Parameters
+        self.BATCH_SIZE = 32
+        self.LR = 0.01                   # learning rate
+        self.EPSILON = 0.9               # greedy policy
+        self.GAMMA = 0.9                 # reward discount
+        self.TARGET_REPLACE_ITER = 100   # target update frequency
+        self.MEMORY_CAPACITY = 2000
+        self.N_ACTIONS = 6
+        self.N_STATES = self.input_size
         #--------------------------------------------------------------------------------
 
-        self.optimizer = optim.RMSprop(self.policy_net.parameters())
-        self.memory = _ReplayMemory(10000)
-
-        self.episode_durations = []
+        self.eval_net, self.target_net = self.model.to(device), self.model.to(device)
+        self.learn_step_counter = 0                                               # for target updating
+        self.memory_counter = 0                                                   # for storing memory
+        self.memory = np.zeros((self.MEMORY_CAPACITY, self.N_STATES * 2 + 2))     # initialize memory
+        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=self.LR)
+        self.loss_func = nn.MSELoss()
 
         if os.path.isfile(self.utils.save_file):
             checkpoint = torch.load(self.utils.save_file)
-            self.policy_net.load_state_dict(checkpoint['policy_net'])
+            self.eval_net.load_state_dict(checkpoint['eval_net'])
             self.target_net.load_state_dict(checkpoint['target_net'])
             print("=> loaded checkpoint '{}'".format(checkpoint['iter']))
 
-    #def optimize_model():
-    def _train(self):
-        if len(self.memory) < self.BATCH_SIZE:
-            return
-        transitions = self.memory.sample(self.BATCH_SIZE)
-        # Transpose the batch (see http://stackoverflow.com/a/19343/3343043 for
-        # detailed explanation).
-        batch = Transition(*zip(*transitions))
+    def store_transition(self, s, a, r, s_):
+        transition = np.hstack((s, [a, r], s_))
+        # replace the old memory with new memory
+        index = self.memory_counter % self.MEMORY_CAPACITY
+        self.memory[index, :] = transition
+        self.memory_counter += 1 
 
-        # This mask indicates whether vector at corresponding index is final or not
-        non_final_mask = torch.tensor(
-                tuple(map(
-                        lambda s: s is not None,
-                        batch.next_state
-                    )), device=device, dtype=torch.uint8)
-
-        non_final_next_states = torch.stack([s for s in batch.next_state
-                                                    if s is not None]).to(device)
-        state_batch  = torch.stack(batch.state).to(device)
-        action_batch = torch.stack(batch.action).to(device)
-        reward_batch = torch.stack(batch.reward).to(device) 
-
-        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-        # columns of actions taken
-        state_action_values = self.policy_net(state_batch).gather(1, action_batch.view(-1, 1))
-
-        next_state_values = torch.zeros(self.BATCH_SIZE, device=device)
-        next_state_values[non_final_mask] = self.target_net(non_final_next_states).detach().max(1)[0]
-        # Compute the expected Q values
-        reward_batch = reward_batch.view(-1)
-        expected_state_action_values = (next_state_values * self.GAMMA) + reward_batch
-
-        # Compute Huber loss
-        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-
-        # Optimize the model
-        self.optimizer.zero_grad()
-        loss.backward()
-        for param in self.policy_net.parameters():
-            param.grad.data.clamp_(-1, 1)
-        self.optimizer.step()
+    def choose_action(self, x):
+        # input only one sample
+        if np.random.uniform() < self.EPSILON:   # greedy
+            actions_value = self.eval_net.forward(x)
+            action = torch.max(actions_value, 1)[1].data.numpy()[0, 0]     # return the argmax
+        else:   # random
+            action = np.random.randint(0, self.N_ACTIONS)
+        return action
     
-    def _select_action(self, state):
-        sample = random.random()
-        eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * \
-            math.exp(-1. * self.step_num / self.EPS_DECAY)
-        self.step_num += 1
-        if sample > eps_threshold:
-            with torch.no_grad():
-                return self.policy_net(state.to(device)).max(0)[1].view(1, 1)
-        else:
-            return torch.tensor([[random.randrange(6)]], device=device, dtype=torch.long)
-    
-    
+    def store_transition(self, s, a, r, s_):
+        transition = np.hstack((s, [a, r], s_))
+        # replace the old memory with new memory
+        index = self.memory_counter % self.MEMORY_CAPACITY
+        self.memory[index, :] = transition
+        self.memory_counter += 1
+
     def act(self, obs, action_space):
         x_torch = self.utils.input(obs) 
-        action = self._select_action(x_torch)
+        action = self.choose_action(x_torch)
         return action
  
--- train_0.py
+++ train_0.py
@@ -9,7 +9,8 @@ from fc_agent import FCAgent
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Hyperparams
-EPISODES = 1
+EPISODES = 300 
+
 
 fc_agent = FCAgent()
 agent_list = [fc_agent, agents.SimpleAgent(), agents.RandomAgent(), agents.SimpleAgent()]
@@ -25,10 +26,31 @@ for an_episode in range(EPISODES):
          
     #-------------------------------------------------------------------
     done  = False
+    memory_stop = False
     while not done:
         #env.render()
         actions = env.act(state); 
+        if fc_agent.is_alive: actions[0] = actions[0].item()
         state, reward, done, info = env.step(actions)
+
+        fca_reward = torch.tensor([float(reward[0])], device=device)
+        fca_action = torch.tensor(actions[0], device=device)
+        # Observe new state
+        last_x = current_x
+        current_x = fc_agent.utils.input(state[0])
+
+        # Store the transition in memory
+        # Game over
+        if done or (not fc_agent.is_alive and not memory_stop): 
+            fc_agent.memory.push(last_x, fca_action, None, fca_reward)
+            memory_stop = True
+        # Game on
+        else:
+            fc_agent.memory.push(last_x, fca_action, current_x, fca_reward)
+            
+        # Perform one step of the optimization (on the target network)
+        fc_agent._train()
+        iter_num += 1
     #-------------------------------------------------------------------
         
     #for agent in agent_list:
@@ -39,4 +61,8 @@ for an_episode in range(EPISODES):
     if 'winners' in info:
         wins[info['winners'][0]] = wins.get(info['winners'][0], 0) + 1 
     print(wins)
+
+    # Update the target network
+    if an_episode % target_update == 0:
+        fc_agent.target_net.load_state_dict(fc_agent.policy_net.state_dict())
 
